24b16ff13e00305f6031a01688b0a425db921ed9
Gu7z <gustavoferri13@gmail.com>
2020-02-17 13:12:51 -0400

    adicionando qrcode

--- src/routes/gh.js
+++ src/routes/gh.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+
+import QRCode from 'react-native-qrcode-svg';
+import { Linking } from 'expo';
 
 const styles = StyleSheet.create({
   center: {
     flex: 1,
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   }
 })
 
-export default function GitHub() {
+export default function FaceBook() {
   return (
     <View style={styles.center} >
-        <Text>GitHub</Text>
+        <View style={{backgroundColor: 'white', padding: 15}} >
+          <TouchableOpacity onPress={()=>{
+            Alert.alert(
+              'Linkedin Gustavo Ferri', 
+              'https://www.facebook.com/gustavo.ferrialixandre',
+              [
+                {
+                  text: 'Cancelar',
+                  onPress: ()=>{console.log('testando')}
+                },
+                {
+                  text: 'Ir até la',
+                  onPress: ()=>{Linking.openURL('https://www.facebook.com/gustavo.ferrialixandre')}
+                }
+              ]
+            )
+          }} >
+            <QRCode
+              value="https://www.facebook.com/gustavo.ferrialixandre"
+            />
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
--- src/routes/linkedin.js
+++ src/routes/linkedin.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+
+import QRCode from 'react-native-qrcode-svg';
+import { Linking } from 'expo';
 
 const styles = StyleSheet.create({
   center: {
     flex: 1,
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   }
 })
 
 export default function FaceBook() {
   return (
     <View style={styles.center} >
-        <Text>Linkedin</Text>
+        <View style={{backgroundColor: 'white', padding: 15}} >
+          <TouchableOpacity onPress={()=>{
+            Alert.alert(
+              'Linkedin Gustavo Ferri', 
+              'https://www.linkedin.com/in/gustavo-ferri/',
+              [
+                {
+                  text: 'Cancelar',
+                  onPress: ()=>{console.log('testando')}
+                },
+                {
+                  text: 'Ir até la',
+                  onPress: ()=>{Linking.openURL('https://www.linkedin.com/in/gustavo-ferri/')}
+                }
+              ]
+            )
+          }} >
+            <QRCode
+              value="https://www.linkedin.com/in/gustavo-ferri/"
+            />
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
